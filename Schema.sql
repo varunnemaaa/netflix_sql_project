51498c14f611232d5998165d7d3b6099319d3c4e
varunnemaaa <144524197+varunnemaaa@users.noreply.github.com>
2025-12-31 20:35:57 +0530

    Clean up Schema.sql by removing comments and select
    Removed SQL comments and select statement from Schema.sql.

---
+++ Schema.sql
@@ -0,0 +1,14 @@
+CREATE TABLE netflix (
+    show_id VARCHAR(20),
+    type VARCHAR(20),
+    title VARCHAR(255),
+    director VARCHAR(255),
+    caste VARCHAR(1000),
+    country VARCHAR(150),
+    date_added DATE,
+    release_year INT,
+    rating VARCHAR(20),
+    duration VARCHAR(50),
+    listed_in VARCHAR(255),
+    description TEXT
+);
